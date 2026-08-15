8119b27bb74cb61139caad8d4fdbaefdb5f0e335
Sitt-Paing <sittp9288@gmail.com>
2026-08-15 18:01:55 +0630

    Update Category.cs

--- Entities/Category.cs
+++ Entities/Category.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
 using Microsoft.EntityFrameworkCore;
 
 namespace Bookshop.Entities;
@@ -11,13 +10,17 @@ namespace Bookshop.Entities;
 public partial class Category
 {
     [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
     public int Id { get; set; }
 
     [StringLength(200)]
     public string Name { get; set; } = null!;
 
-    public string Description { get; set; } = null!;
+    public string? Description { get; set; }
+
+    [StringLength(500)]
+    public string? ImageUrl { get; set; }
+
+    public bool IsActive { get; set; }
 
     [Column(TypeName = "datetime")]
     public DateTime? CreatedOn { get; set; }
@@ -36,8 +39,4 @@ public partial class Category
 
     [StringLength(100)]
     public string? DeletedBy { get; set; }
-
-    [JsonIgnore]
-    [InverseProperty("Category")]
-    public virtual ICollection<Book> Books { get; set; } = new List<Book>();
 }
